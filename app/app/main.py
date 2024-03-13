c981a28c837af6889ad9b44ac3a9dd858deb07b5
JuanGiraldoB <jugiraldob@unal.edu.co>
2024-03-13 18:13:36 -0500

    fixed imports

--- app/app/main.py
+++ app/app/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
 from app.models import Base
-from .routers import dogs, users
-from .database import engine
+from app.routers import dogs, users
+from app.database import engine
 
 Base.metadata.create_all(bind=engine)
 
