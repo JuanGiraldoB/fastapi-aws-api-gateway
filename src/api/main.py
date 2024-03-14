0ec69f1e86c5908750666aca559e45291dfb812c
JuanGiraldoB <jugiraldob@unal.edu.co>
2024-03-14 16:05:35 -0500

    Added cors

--- src/api/main.py
+++ src/api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 from mangum import Mangum
 
@@ -9,6 +10,14 @@ from api.database import engine
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(dogs.router)
